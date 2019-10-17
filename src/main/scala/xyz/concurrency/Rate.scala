66c8ce72a644e79a83b07817f26d080bde055e10
Siew Li Meng <siewlimeng7@gmail.com>
2019-10-17 11:15:48 +0200

    Add rate features (#4)

--- src/main/scala/xyz/concurrency/Rate.scala
+++ src/main/scala/xyz/concurrency/Rate.scala
@@ -2,7 +2,10 @@ package xyz.concurrency
 
 import play.api.libs.json._
 
-final case class Rate(from: String, to: String, rate: Double, timestamp: Long)
+final case class Rate(from: String, to: String, rate: Double, timestamp: Long) {
+  require(from != to, "`from` and `to` cannot be the same value.")
+  require(rate > 0, "`rate` must greater than 0.")
+}
 
 object Rate {
   implicit val implicitWrites: Writes[Rate] = (rate: Rate) => {
@@ -28,6 +31,26 @@ object RateList {
   }
 
   def apply(rates: List[Rate]): RateList = {
-    RateList(rates, Some(rates.head), Some(rates.head), Some(rates.head), Some(rates.head))
+    val earliestRate = rates.map(_.timestamp) match {
+      case rs if rs.isEmpty => None
+      case rs               => rates.find(_.timestamp == rs.min)
+    }
+
+    val latestRate = rates.map(_.timestamp) match {
+      case rs if rs.isEmpty => None
+      case rs               => rates.find(_.timestamp == rs.max)
+    }
+
+    val lowestRate = rates.map(_.rate) match {
+      case rs if rs.isEmpty => None
+      case rs               => rates.find(_.rate == rs.min)
+    }
+
+    val highestRate = rates.map(_.rate) match {
+      case rs if rs.isEmpty => None
+      case rs               => rates.find(_.rate == rs.max)
+    }
+
+    RateList(rates, earliestRate, latestRate, lowestRate, highestRate)
   }
 }
--- src/test/scala/xyz/concurrency/RateSpec.scala
+++ src/test/scala/xyz/concurrency/RateSpec.scala
@@ -4,6 +4,19 @@ import org.scalatest._
 
 class RateSpec extends FlatSpec with Matchers {
 
+  val getRate: Double => Rate = (rate: Double) => Rate("EUR", "MYR", rate, 1L)
+  val getRateByTimestamp: Long => Rate = (timestamp: Long) => Rate("EUR", "MYR", 1, timestamp)
+
+  "A rate" should "not has a same from and to" in {
+    an[IllegalArgumentException] should be thrownBy Rate("A", "A", 1, 1)
+    noException should be thrownBy Rate("A", "B", 1, 1)
+  }
+
+  it should "has a rate of greater than zero" in {
+    an[IllegalArgumentException] should be thrownBy Rate("A", "B", 0, 1)
+    an[IllegalArgumentException] should be thrownBy Rate("A", "B", -1, 1)
+  }
+
   "A rate list" should "be created with list of rate" in {
     val rates: List[Rate] = (1 to 100).map(n => Rate("MYR", "EUR", n, n)).toList
     val rateList: RateList = RateList(rates)
@@ -11,10 +24,51 @@ class RateSpec extends FlatSpec with Matchers {
     rateList.rates.size should be(rates.size)
   }
 
-  it should "has a lower rate" in {
-    val rate = Rate("EUR", "MYR", 1, 1L)
-    val rateList = RateList(List(rate))
+  it should "has a lowest rate" in {
+    val rates = RateList(List(2, 3, 2, 3, 1, 4).map(r => getRate(r)))
+    rates.lowestRate should be(Some(getRate(1)))
+  }
 
-    rateList.lowestRate should be(Some(rate))
+  it should "pick the first lowest rate" in {
+    val rates = RateList(List(1, 3, 2, 3, 1, 4).map(r => getRate(r)))
+    rates.lowestRate should be(Some(getRate(1)))
+  }
+
+  it should "be none when no rates" in {
+    val rates = RateList(List())
+    rates.lowestRate should be(None)
+    rates.highestRate should be(None)
+    rates.earliestRate should be(None)
+    rates.latestRate should be(None)
+  }
+
+  it should "has a highest rate" in {
+    val rates = RateList(List(2, 3, 2, 3, 1, 4).map(r => getRate(r)))
+    rates.highestRate should be(Some(getRate(4)))
+  }
+
+  it should "pick the first highest rate" in {
+    val rates = RateList(List(1, 4, 2, 3, 1, 4).map(r => getRate(r)))
+    rates.highestRate should be(Some(getRate(4)))
+  }
+
+  it should "has a earliest timestamp" in {
+    val rates = RateList(List(2, 3, 2, 3, 1, 4).map(t => getRateByTimestamp(t)))
+    rates.earliestRate should be(Some(getRateByTimestamp(1)))
+  }
+
+  it should "pick the first earliest rate" in {
+    val rates = RateList(List(2, 3, 1, 3, 1, 4).map(t => getRateByTimestamp(t)))
+    rates.earliestRate should be(Some(getRateByTimestamp(1)))
+  }
+
+  it should "has a latest timestamp" in {
+    val rates = RateList(List(2, 3, 2, 3, 1, 4).map(t => getRateByTimestamp(t)))
+    rates.latestRate should be(Some(getRateByTimestamp(4)))
+  }
+
+  it should "pick the first latest rate" in {
+    val rates = RateList(List(4, 3, 1, 3, 1, 4).map(t => getRateByTimestamp(t)))
+    rates.latestRate should be(Some(getRateByTimestamp(4)))
   }
 }
